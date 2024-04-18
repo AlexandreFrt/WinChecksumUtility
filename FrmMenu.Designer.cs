d0061d5e9353582794bd3e9a83060bdda9a06256
Alexandre <foretalexandre37@gmail.com>
2024-04-18 16:52:08 +0200

    add: Add Drag & Drop events

---
+++ FrmMenu.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WinSHA256
+{
+    partial class FrmMenu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutMain = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutMain
+            // 
+            this.tableLayoutMain.AllowDrop = true;
+            this.tableLayoutMain.ColumnCount = 2;
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutMain.Name = "tableLayoutMain";
+            this.tableLayoutMain.RowCount = 2;
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutMain.TabIndex = 0;
+            // 
+            // FrmMenu
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutMain);
+            this.Name = "FrmMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "WinSHA256 Utility";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmMenu_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmMenu_DragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutMain;
+    }
+}
+
---
+++ FrmMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace WinSHA256
+{
+    public partial class FrmMenu : Form
+    {
+        public FrmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMenu_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void FrmMenu_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            // Si il n'y a qu'un seul fichier dans le drag
+            if (files.Length == 1)
+            {
+                // Initialise l'object SHA256
+                using (SHA256 SHA256 = SHA256.Create())
+                {
+                    using (FileStream fileStream;)
+                    {
+
+                    }
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
