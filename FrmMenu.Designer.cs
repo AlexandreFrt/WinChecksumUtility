e680d52f7234e46b4760f40721f9c8f07a5ab9e6
Alexandre <foretalexandre37@gmail.com>
2024-04-19 11:03:24 +0200

    add: Add SHA256 Compute

--- FrmMenu.Designer.cs
+++ FrmMenu.Designer.cs
@@ -29,35 +29,200 @@
         private void InitializeComponent()
         {
             this.tableLayoutMain = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBoxOriginalChecksumValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFileHash = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.buttonImport = new System.Windows.Forms.Button();
+            this.buttonCheckChecksum = new System.Windows.Forms.Button();
+            this.buttonComputeChecksum = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.tableLayoutMain.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutMain
             // 
             this.tableLayoutMain.AllowDrop = true;
-            this.tableLayoutMain.ColumnCount = 2;
-            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutMain.ColumnCount = 1;
+            this.tableLayoutMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutMain.Controls.Add(this.tableLayoutPanel1, 0, 0);
             this.tableLayoutMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutMain.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutMain.Name = "tableLayoutMain";
-            this.tableLayoutMain.RowCount = 2;
-            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutMain.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutMain.RowCount = 1;
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 99.99999F));
+            this.tableLayoutMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutMain.Size = new System.Drawing.Size(698, 331);
             this.tableLayoutMain.TabIndex = 0;
+            this.tableLayoutMain.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmMenu_DragDrop);
+            this.tableLayoutMain.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmMenu_DragEnter);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 87.03466F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.96534F));
+            this.tableLayoutPanel1.Controls.Add(this.textBoxOriginalChecksumValue, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxFileHash, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBoxFilePath, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonImport, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCheckChecksum, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.buttonComputeChecksum, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.61724F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19.94616F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.613456F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.51308F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.617241F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.51072F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.617241F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.56486F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(692, 325);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // textBoxOriginalChecksumValue
+            // 
+            this.textBoxOriginalChecksumValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxOriginalChecksumValue.Location = new System.Drawing.Point(3, 257);
+            this.textBoxOriginalChecksumValue.Name = "textBoxOriginalChecksumValue";
+            this.textBoxOriginalChecksumValue.Size = new System.Drawing.Size(594, 23);
+            this.textBoxOriginalChecksumValue.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Checksum du fichier original :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(594, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selectionner un fichier pour calculer le checksum (ou vous pouvez glisser déposer" +
+    " un fichier)";
+            // 
+            // textBoxFileHash
+            // 
+            this.textBoxFileHash.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFileHash.Location = new System.Drawing.Point(3, 183);
+            this.textBoxFileHash.Name = "textBoxFileHash";
+            this.textBoxFileHash.Size = new System.Drawing.Size(594, 23);
+            this.textBoxFileHash.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Checksum du fichier actuel :";
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFilePath.Location = new System.Drawing.Point(3, 24);
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(594, 23);
+            this.textBoxFilePath.TabIndex = 9;
+            // 
+            // buttonImport
+            // 
+            this.buttonImport.Location = new System.Drawing.Point(605, 24);
+            this.buttonImport.Name = "buttonImport";
+            this.buttonImport.Size = new System.Drawing.Size(75, 23);
+            this.buttonImport.TabIndex = 6;
+            this.buttonImport.Text = "Importer ...";
+            this.buttonImport.UseVisualStyleBackColor = true;
+            this.buttonImport.Click += new System.EventHandler(this.buttonImport_Click);
+            // 
+            // buttonCheckChecksum
+            // 
+            this.buttonCheckChecksum.Location = new System.Drawing.Point(605, 257);
+            this.buttonCheckChecksum.Name = "buttonCheckChecksum";
+            this.buttonCheckChecksum.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheckChecksum.TabIndex = 7;
+            this.buttonCheckChecksum.Text = "Vérifier ...";
+            this.buttonCheckChecksum.UseVisualStyleBackColor = true;
+            this.buttonCheckChecksum.Click += new System.EventHandler(this.buttonCheckChecksum_Click);
+            // 
+            // buttonComputeChecksum
+            // 
+            this.buttonComputeChecksum.Location = new System.Drawing.Point(605, 183);
+            this.buttonComputeChecksum.Name = "buttonComputeChecksum";
+            this.buttonComputeChecksum.Size = new System.Drawing.Size(75, 23);
+            this.buttonComputeChecksum.TabIndex = 7;
+            this.buttonComputeChecksum.Text = "Recalculer";
+            this.buttonComputeChecksum.UseVisualStyleBackColor = true;
+            this.buttonComputeChecksum.Click += new System.EventHandler(this.buttonComputeChecksum_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "SHA-256",
+            "SHA-512",
+            "MD5"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 109);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.Text = "SHA-256";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(264, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Selectionner un algorithme de hashage :";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
             // 
             // FrmMenu
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(698, 331);
             this.Controls.Add(this.tableLayoutMain);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(809, 406);
             this.Name = "FrmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "WinSHA256 Utility";
+            this.Text = "WinChecksum Utility";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmMenu_DragDrop);
             this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmMenu_DragEnter);
+            this.tableLayoutMain.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -65,6 +230,19 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutMain;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBoxFileHash;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxOriginalChecksumValue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonImport;
+        private System.Windows.Forms.Button buttonCheckChecksum;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button buttonComputeChecksum;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- FrmMenu.cs
+++ FrmMenu.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace WinSHA256
 {
@@ -31,19 +32,108 @@ namespace WinSHA256
             // Si il n'y a qu'un seul fichier dans le drag
             if (files.Length == 1)
             {
-                // Initialise l'object SHA256
-                using (SHA256 SHA256 = SHA256.Create())
-                {
-                    using (FileStream fileStream;)
-                    {
-
-                    }
-                }
+                string sha256 = ComputeSha256(files);
+                textBoxFileHash.Text = sha256;
+                textBoxFilePath.Text = files[0];
             }
             else
             {
-
+                // Si plusieurs fichiers ont été drop alors on affiche une erreur
+                MessageBox.Show("Veuillez selectionner un seul fichier à la fois", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        private string ComputeSha256(string[] files)
+        {
+            string sha256Hash = "";
+
+            // Initialise l'object SHA256
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                if (File.Exists(files[0]))
+                {
+                    FileInfo fileInfo = new FileInfo(files[0]);
+
+                    using (FileStream fileStream = fileInfo.Open(FileMode.Open))
+                    {
+                        try
+                        {
+                            // Créer un fileStream pour le fichier
+                            // Être sur qu'il soit positionné au début du stream
+                            fileStream.Position = 0;
+
+                            byte[] hashValue = sha256.ComputeHash(fileStream);
+
+                            sha256Hash = NormalizeSha256Hash(hashValue);
+                        }
+                        catch (IOException error)
+                        {
+                            MessageBox.Show(error.ToString(), "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        catch (UnauthorizedAccessException error)
+                        {
+                            MessageBox.Show(error.ToString(), "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Le fichier : " + files[0] + " n'existe pas", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            return sha256Hash;
+        }
+
+        // Procédure qui renormalise le hash, supprime les -, et le convertit en minuscule
+        private string NormalizeSha256Hash(byte[] bytes)
+        {
+            string sha256 = BitConverter.ToString(bytes);
+            sha256 = sha256.ToLower();
+            sha256 = sha256.Replace("-", "");
+
+            return sha256;
+        }
+
+        private void buttonImport_Click(object sender, EventArgs e)
+        {
+            openFileDialog.FileName = null;
+            openFileDialog.ShowDialog();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            string[] files = openFileDialog.FileNames;
+            string sha256 = ComputeSha256(files);
+            textBoxFileHash.Text = sha256;
+            textBoxFilePath.Text = files[0];
+        }
+
+        private void buttonCheckChecksum_Click(object sender, EventArgs e)
+        {
+            if (!textBoxFileHash.Text.Equals(textBoxOriginalChecksumValue.Text))
+            {
+                MessageBox.Show("Le checksum du fichier est : " + textBoxFileHash.Text + "\nLe checksum original du fichier est : " + textBoxOriginalChecksumValue.Text + "\n\nLES CHECKSUM NE SE CORRESPONDENT PAS !!!", "Vérification du checksum", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Le checksum du fichier est : " + textBoxFileHash.Text + "\nLe checksum original du fichier est : " + textBoxOriginalChecksumValue.Text + "\n\nLes checksum se correspondent.", "Vérification du checksum", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonComputeChecksum_Click(object sender, EventArgs e)
+        {
+            string[] files = new string[1];
+            files[0] = textBoxFilePath.Text;
+            files[0] = RemoveInvisibleCharacters(files[0]);
+            string sha256Hash = ComputeSha256(files);
+            textBoxFileHash.Text = sha256Hash;
+        }
+
+        // Fonction qui utilise une expression régulière pour remplacer les caractères invisibles et retourne la valeur une fois modifié
+        public static string RemoveInvisibleCharacters(string input)
+        {
+            return Regex.Replace(input, @"\p{C}", "");
         }
     }
 }
